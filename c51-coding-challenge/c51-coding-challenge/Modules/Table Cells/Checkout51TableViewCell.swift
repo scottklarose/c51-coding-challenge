3fa66b13d4d4db71d492e5a1396cabce965fb165
Scott Larose <scott.klarose@gmail.com>
2019-07-28 22:42:27 -0400

    Added the table view cell.

---
+++ c51-coding-challenge/c51-coding-challenge/Modules/Table Cells/Checkout51TableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  Checkout51TableViewCell.swift
+//  c51-coding-challenge
+//
+
+import UIKit
+
+
+class Checkout51TableViewCell: UITableViewCell {
+    @IBOutlet weak var offerImage: UIImageView!
+    @IBOutlet weak var offerName: UILabel!
+    @IBOutlet weak var cashBack: UILabel!
+}
