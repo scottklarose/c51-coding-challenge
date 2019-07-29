a99d67226d0ff83a73df5c9409eafc6980070f7d
Scott Larose <scott.klarose@gmail.com>
2019-07-28 21:24:25 -0400

    Initial setup of the project.

---
+++ c51-coding-challenge/c51-coding-challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  c51-coding-challenge
+//
+//  Created by Scott Larose on 2019-07-28.
+//  Copyright © 2019 ScottLarose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ c51-coding-challenge/c51-coding-challengeTests/c51_coding_challengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  c51_coding_challengeTests.swift
+//  c51-coding-challengeTests
+//
+//  Created by Scott Larose on 2019-07-28.
+//  Copyright © 2019 ScottLarose. All rights reserved.
+//
+
+import XCTest
+@testable import c51_coding_challenge
+
+class c51_coding_challengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
